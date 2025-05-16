794d5f14168cd7c6791d94e95d32a8cdfa5fb7ed
Deka-ops <gnwabuikwuchori@byupathway.edu>
2025-05-16 20:28:40 +0100

    updated journal

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        journal thejournal = new journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
+        
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
---
+++ week02/Journal/journal.cs
@@ -0,0 +1,19 @@
+public class journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+        
+    }
+}
